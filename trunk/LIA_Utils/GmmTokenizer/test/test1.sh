bf9797c3a47e4de9f75c433190004b2825c56c79
admin <admin@1290fa5e-fd49-494a-a208-9d5a245c2233>
2007-12-05 11:37:01 +0000

    trunk creation

--- trunk/LIA_Utils/GmmTokenizer/test/test1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# compare the resulting test1.sym with test1.sym.ref
-../GmmTokenizer --config GmmTokenizer.cfg --verbose true
-../GmmTokenizer --config GmmTokenizer.cfg --confusionMatrix --verbose true --matrixOutputName mce_matrix.mat
